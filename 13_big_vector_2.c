b2b99b154567e710de0843ab8b0cbaf752587a6c
Jun He <junnhe@gmail.com>
2011-03-07 19:34:45 -0500

    add error handle

--- 13_big_vector_2.c
+++ 13_big_vector_2.c
@@ -9,7 +9,7 @@
 
 #define BLOCK_CNT   1024
 #define BLOCK_LEN   1024
-#define STRIDE_LEN  (4096*1024)
+#define STRIDE_LEN  (4096)
 
 int rank, size;
 
@@ -19,9 +19,9 @@ int main (argc, argv)
 {
     MPI_File fh;
     MPI_Status status;
-    char rank_char[128];
     char *junk;
     int i,j;
+    int ret;
     MPI_Datatype vec_type;
 
     MPI_Init (&argc, &argv);	/* starts MPI */
@@ -29,7 +29,11 @@ int main (argc, argv)
     MPI_Comm_size (MPI_COMM_WORLD, &size);	/* get number of processes */
 
     /* This is a collective call, which means every process does the same thing, opens the same file */
-    MPI_File_open( MPI_COMM_WORLD, "/tmp/hellovirginfile2", MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &fh );
+    ret = MPI_File_open( MPI_COMM_WORLD, "/tmp/hellovirginfile2", MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &fh );
+    if ( ret != MPI_SUCCESS ) {
+        printf("Failed to open file");
+        goto final;
+    }
     
 
     MPI_Type_vector( BLOCK_CNT, BLOCK_LEN, STRIDE_LEN, MPI_CHAR, &vec_type );
@@ -37,15 +41,22 @@ int main (argc, argv)
 
     
     /* write to file */
-    if ( rank == 0 && argc > 1 && (strcmp(argv[1], "createfile") == 0) ) {
+    if ( argc > 1 && (strcmp(argv[1], "createfile") == 0) ) {
         printf( "Create file...\n" );
         junk = malloc ( STRIDE_LEN  );
         for ( i = 0 ; i < STRIDE_LEN ; i++ ) {
             junk[i] = (char) i + '0';
         }
         for ( i = 0 ; i < BLOCK_CNT ; i++ ) {
-            MPI_File_write( fh, junk, STRIDE_LEN, MPI_CHAR, &status);
+            ret = MPI_File_write( fh, junk, STRIDE_LEN, MPI_CHAR, &status);
+            if ( ret != MPI_SUCCESS ) {
+                printf("failed to write file\n");
+                goto final;
+            }
         }
+        free(junk);
+    } else {
+        printf("read file only\n");
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
@@ -53,22 +64,30 @@ int main (argc, argv)
 
     MPI_File_set_view(fh, 0, MPI_CHAR, vec_type, "native", MPI_INFO_NULL);
     
+    
     long readsize = BLOCK_CNT*BLOCK_LEN;
+    char *buffer = malloc (readsize);
+    if (buffer == NULL) {
+        printf("Can't allocate memory for buffer.\n");
+        goto final;
+    }
+
+    
     double start = MPI_Wtime();
-    int ret = MPI_File_read( fh, rank_char, 1, MPI_CHAR, &status);
+    MPI_File_read( fh, buffer, readsize, MPI_CHAR, &status);
     double end = MPI_Wtime();
     double total = end - start;
 
     if ( ret != MPI_SUCCESS ) {
         printf("File read failed\n");
+        goto final;
     } else {
         printf( "Read success. Bandwidth:%lf\n", readsize/(total*1024*1024) );
     }
 
 
-    free(junk);
     MPI_File_close(&fh);
-
+final:
     MPI_Finalize();
     return 0;
 }
